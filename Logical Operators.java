effdfacf29d2d3b984f9b72e38479da17340a02c
Deepak-sangaraju <sreddydeepak2006@gmail.com>
2025-11-17 18:56:43 +0530

    Create Logical Operators.java

---
+++ Logical Operators.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class VotingEligibility {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // Input age
+        System.out.print("Enter age: ");
+        int age = sc.nextInt();
+
+        // Input citizenship (true/false)
+        System.out.print("Are you a citizen? (true/false): ");
+        boolean isCitizen = sc.nextBoolean();
+
+        // Check eligibility
+        if (age >= 18 && isCitizen) {
+            System.out.println("You are eligible to vote.");
+        } else {
+            System.out.println("You are NOT eligible to vote.");
+        }
+
+        sc.close();
+    }
+}
