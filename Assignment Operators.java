fe5df8a05f5c5c3b13360fa033fb5a8d5658e817
Deepak-sangaraju <sreddydeepak2006@gmail.com>
2025-11-17 18:57:43 +0530

    Add AssignmentOperatorsDemo class with examples

---
+++ Assignment Operators.java
@@ -0,0 +1,19 @@
+public class AssignmentOperatorsDemo {
+    public static void main(String[] args) {
+
+        int num = 10;  // initial value
+        System.out.println("Initial value: " + num);
+
+        // Using +=
+        num += 5;  
+        System.out.println("After num += 5: " + num);
+
+        // Using -=
+        num -= 3;  
+        System.out.println("After num -= 3: " + num);
+
+        // Using *=
+        num *= 2;  
+        System.out.println("After num *= 2: " + num);
+    }
+}
