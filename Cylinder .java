6b3523b36fcc08fdb6f180c0945f6d19ab86a5d6
Deepak-sangaraju <sreddydeepak2006@gmail.com>
2025-11-17 19:21:01 +0530

    Create  Cylinder .java

---
+++ Cylinder .java
@@ -0,0 +1,51 @@
+class Cylinder {
+
+    private double radius;
+    private double height;
+
+    // Setter for radius
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    // Getter for radius
+    public double getRadius() {
+        return radius;
+    }
+
+    // Setter for height
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    // Getter for height
+    public double getHeight() {
+        return height;
+    }
+
+    // Method to calculate volume
+    public double getVolume() {
+        return Math.PI * radius * radius * height;
+    }
+
+    // Method to calculate surface area
+    public double getSurfaceArea() {
+        return 2 * Math.PI * radius * (radius + height);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        Cylinder c = new Cylinder();
+
+        c.setRadius(5.0);
+        c.setHeight(10.0);
+
+        System.out.println("Radius: " + c.getRadius());
+        System.out.println("Height: " + c.getHeight());
+
+        System.out.println("Volume: " + c.getVolume());
+        System.out.println("Surface Area: " + c.getSurfaceArea());
+    }
+}
